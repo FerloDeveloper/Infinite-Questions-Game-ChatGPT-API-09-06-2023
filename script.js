ad2da5457fd5ee910fb3f38c37952eca9122d698
FerloDeveloper <gianluca.laferla.dev@gmail.com>
2023-06-09 12:32:34 +0200

    perfect loader

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const MODEL = "gpt-3.5-turbo";
-const API_KEY = "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"; 
+// const API_KEY = "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"; 
 const input = document.querySelector(".input");
 let inputValue = input.value;
 const messageArea = document.querySelector(`.message-area`);
@@ -74,6 +74,7 @@ function getRandomAction() {
 }
 
 btnSend.addEventListener("click", function () {
+    messageArea.innerHTML = `<img class="loader" src="loader.gif" alt="">`;
 	inputValue = input.value;
 	generateResponse(inputValue);
 	input.value = "";
